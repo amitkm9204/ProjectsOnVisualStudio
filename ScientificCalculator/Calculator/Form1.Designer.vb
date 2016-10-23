45dd52d938b11ae638f00d54c9effa412661ee99
AMIT KUMAR <AMIT KUMAR>
2016-10-23 10:07:31 +0530

    adding the tested basic calculator

---
+++ ScientificCalculator/Calculator/Form1.Designer.vb
@@ -0,0 +1,288 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Button11 = New System.Windows.Forms.Button()
+        Me.Button12 = New System.Windows.Forms.Button()
+        Me.Button13 = New System.Windows.Forms.Button()
+        Me.Button14 = New System.Windows.Forms.Button()
+        Me.Button15 = New System.Windows.Forms.Button()
+        Me.Button16 = New System.Windows.Forms.Button()
+        Me.Button17 = New System.Windows.Forms.Button()
+        Me.Button18 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(53, 305)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(100, 46)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "0"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(53, 253)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(47, 46)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "1"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(106, 253)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(47, 46)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "2"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.Location = New System.Drawing.Point(161, 253)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(47, 46)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "3"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button5.Location = New System.Drawing.Point(53, 200)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(47, 46)
+        Me.Button5.TabIndex = 4
+        Me.Button5.Text = "4"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button6.Location = New System.Drawing.Point(106, 200)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(47, 46)
+        Me.Button6.TabIndex = 5
+        Me.Button6.Text = "5"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button7.Location = New System.Drawing.Point(106, 147)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(47, 46)
+        Me.Button7.TabIndex = 6
+        Me.Button7.Text = "8"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button8.Location = New System.Drawing.Point(160, 200)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(47, 46)
+        Me.Button8.TabIndex = 7
+        Me.Button8.Text = "6"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button9.Location = New System.Drawing.Point(53, 147)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(47, 46)
+        Me.Button9.TabIndex = 8
+        Me.Button9.Text = "7"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Button10
+        '
+        Me.Button10.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button10.Location = New System.Drawing.Point(160, 148)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(47, 46)
+        Me.Button10.TabIndex = 9
+        Me.Button10.Text = "9"
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'Button11
+        '
+        Me.Button11.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button11.Location = New System.Drawing.Point(159, 305)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(47, 46)
+        Me.Button11.TabIndex = 10
+        Me.Button11.Text = "."
+        Me.Button11.UseVisualStyleBackColor = True
+        '
+        'Button12
+        '
+        Me.Button12.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button12.Location = New System.Drawing.Point(214, 253)
+        Me.Button12.Name = "Button12"
+        Me.Button12.Size = New System.Drawing.Size(48, 98)
+        Me.Button12.TabIndex = 11
+        Me.Button12.Text = "="
+        Me.Button12.UseVisualStyleBackColor = True
+        '
+        'Button13
+        '
+        Me.Button13.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button13.Location = New System.Drawing.Point(215, 200)
+        Me.Button13.Name = "Button13"
+        Me.Button13.Size = New System.Drawing.Size(47, 46)
+        Me.Button13.TabIndex = 12
+        Me.Button13.Text = "+"
+        Me.Button13.UseVisualStyleBackColor = True
+        '
+        'Button14
+        '
+        Me.Button14.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button14.Location = New System.Drawing.Point(214, 147)
+        Me.Button14.Name = "Button14"
+        Me.Button14.Size = New System.Drawing.Size(47, 46)
+        Me.Button14.TabIndex = 13
+        Me.Button14.Text = "-"
+        Me.Button14.UseVisualStyleBackColor = True
+        '
+        'Button15
+        '
+        Me.Button15.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button15.Location = New System.Drawing.Point(53, 94)
+        Me.Button15.Name = "Button15"
+        Me.Button15.Size = New System.Drawing.Size(47, 46)
+        Me.Button15.TabIndex = 14
+        Me.Button15.Text = "*"
+        Me.Button15.UseVisualStyleBackColor = True
+        '
+        'Button16
+        '
+        Me.Button16.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button16.Location = New System.Drawing.Point(106, 94)
+        Me.Button16.Name = "Button16"
+        Me.Button16.Size = New System.Drawing.Size(47, 46)
+        Me.Button16.TabIndex = 15
+        Me.Button16.Text = "/"
+        Me.Button16.UseVisualStyleBackColor = True
+        '
+        'Button17
+        '
+        Me.Button17.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button17.Location = New System.Drawing.Point(160, 94)
+        Me.Button17.Name = "Button17"
+        Me.Button17.Size = New System.Drawing.Size(47, 46)
+        Me.Button17.TabIndex = 16
+        Me.Button17.Text = "1/x"
+        Me.Button17.UseVisualStyleBackColor = True
+        '
+        'Button18
+        '
+        Me.Button18.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Bold)
+        Me.Button18.Location = New System.Drawing.Point(214, 94)
+        Me.Button18.Name = "Button18"
+        Me.Button18.Size = New System.Drawing.Size(47, 46)
+        Me.Button18.TabIndex = 17
+        Me.Button18.Text = "C"
+        Me.Button18.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(53, 37)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(208, 35)
+        Me.TextBox1.TabIndex = 18
+        Me.TextBox1.Text = "0"
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(307, 373)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button18)
+        Me.Controls.Add(Me.Button17)
+        Me.Controls.Add(Me.Button16)
+        Me.Controls.Add(Me.Button15)
+        Me.Controls.Add(Me.Button14)
+        Me.Controls.Add(Me.Button13)
+        Me.Controls.Add(Me.Button12)
+        Me.Controls.Add(Me.Button11)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+    Friend WithEvents Button7 As System.Windows.Forms.Button
+    Friend WithEvents Button8 As System.Windows.Forms.Button
+    Friend WithEvents Button9 As System.Windows.Forms.Button
+    Friend WithEvents Button10 As System.Windows.Forms.Button
+    Friend WithEvents Button11 As System.Windows.Forms.Button
+    Friend WithEvents Button12 As System.Windows.Forms.Button
+    Friend WithEvents Button13 As System.Windows.Forms.Button
+    Friend WithEvents Button14 As System.Windows.Forms.Button
+    Friend WithEvents Button15 As System.Windows.Forms.Button
+    Friend WithEvents Button16 As System.Windows.Forms.Button
+    Friend WithEvents Button17 As System.Windows.Forms.Button
+    Friend WithEvents Button18 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+
+End Class
---
+++ ScientificCalculator/Calculator/Form1.vb
@@ -0,0 +1,163 @@
+﻿Public Class Form1
+    Dim temp As String = ""
+    Dim first As Double = 0
+    Dim second As Double = 0
+    Dim op As String
+    Dim tempop As Double
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        temp = TextBox1.Text
+        If temp = "0" Or temp = "+" Or temp = "-" Or temp = "*" Or temp = "/" Then
+            TextBox1.Text = "0"
+        Else
+            TextBox1.Text = temp + "0"
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        temp = TextBox1.Text
+        If temp = "0" Or temp = "+" Or temp = "-" Or temp = "*" Or temp = "/" Then
+            TextBox1.Text = "1"
+        Else
+            TextBox1.Text = temp + "1"
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        temp = TextBox1.Text
+        If temp = "0" Or temp = "+" Or temp = "-" Or temp = "*" Or temp = "/" Then
+            TextBox1.Text = "2"
+        Else
+            TextBox1.Text = temp + "2"
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        temp = TextBox1.Text
+        If temp = "0" Or temp = "+" Or temp = "-" Or temp = "*" Or temp = "/" Then
+            TextBox1.Text = "3"
+        Else
+            TextBox1.Text = temp + "3"
+        End If
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        temp = TextBox1.Text
+        If temp = "0" Or temp = "+" Or temp = "-" Or temp = "*" Or temp = "/" Then
+            TextBox1.Text = "4"
+        Else
+            TextBox1.Text = temp + "4"
+        End If
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        temp = TextBox1.Text
+        If temp = "0" Or temp = "+" Or temp = "-" Or temp = "*" Or temp = "/" Then
+            TextBox1.Text = "5"
+        Else
+            TextBox1.Text = temp + "5"
+        End If
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        temp = TextBox1.Text
+        If temp = "0" Or temp = "+" Or temp = "-" Or temp = "*" Or temp = "/" Then
+            TextBox1.Text = "6"
+        Else
+            TextBox1.Text = temp + "6"
+        End If
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        temp = TextBox1.Text
+        If temp = "0" Or temp = "+" Or temp = "-" Or temp = "*" Or temp = "/" Then
+            TextBox1.Text = "7"
+        Else
+            TextBox1.Text = temp + "7"
+        End If
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        temp = TextBox1.Text
+        If temp = "0" Or temp = "+" Or temp = "-" Or temp = "*" Or temp = "/" Then
+            TextBox1.Text = "8"
+        Else
+            TextBox1.Text = temp + "9"
+        End If
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        temp = TextBox1.Text
+        If temp = "0" Or temp = "+" Or temp = "-" Or temp = "*" Or temp = "/" Then
+            TextBox1.Text = "9"
+        Else
+            TextBox1.Text = temp + "9"
+        End If
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+        temp = TextBox1.Text
+        If temp = "0" Or temp = "+" Or temp = "-" Or temp = "*" Or temp = "/" Then
+            TextBox1.Text = "0."
+        Else
+            TextBox1.Text = temp + "."
+        End If
+    End Sub
+
+    Private Sub Button18_Click(sender As Object, e As EventArgs) Handles Button18.Click
+        TextBox1.Text = "0"
+    End Sub
+
+    Private Sub Button13_Click(sender As Object, e As EventArgs) Handles Button13.Click
+        first = CDbl(TextBox1.Text)
+        TextBox1.Text = "+"
+        op = "+"
+    End Sub
+
+    Private Sub Button14_Click(sender As Object, e As EventArgs) Handles Button14.Click
+        first = CDbl(TextBox1.Text)
+        TextBox1.Text = "-"
+        op = "-"
+    End Sub
+
+    Private Sub Button15_Click(sender As Object, e As EventArgs) Handles Button15.Click
+        first = CDbl(TextBox1.Text)
+        TextBox1.Text = "*"
+        op = "*"
+    End Sub
+
+    Private Sub Button16_Click(sender As Object, e As EventArgs) Handles Button16.Click
+        first = CDbl(TextBox1.Text)
+        TextBox1.Text = "/"
+        op = "/"
+    End Sub
+
+    Private Sub Button17_Click(sender As Object, e As EventArgs) Handles Button17.Click
+        first = CDbl(TextBox1.Text)
+        TextBox1.Text = 1 / first
+    End Sub
+
+    Private Sub Button12_Click(sender As Object, e As EventArgs) Handles Button12.Click
+        second = CDbl(TextBox1.Text)
+        If op = "+" Then
+            TextBox1.Text = first + second
+        ElseIf op = "-" Then
+            If first > second Then
+                tempop = first - second
+            Else
+                tempop = second - first
+            End If
+            TextBox1.Text = "-" + tempop
+        ElseIf op = "*" Then
+            TextBox1.Text = first * second
+        ElseIf op = "/" Then
+            If second = 0 Then
+                TextBox1.Text = "Error"
+            Else
+                TextBox1.Text = first / second
+            End If
+
+        End If
+
+
+    End Sub
+End Class
